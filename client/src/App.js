10a7c8f05da782df2ee7e26d5d96aff7693847a5
Michael Shing <michael.shing@bt.com>
2018-04-14 13:24:38 +0100

    Add scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App__header">
+          <img src={logo} className="App__logo" alt="logo" />
+          <h1 className="App__title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="App__intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
