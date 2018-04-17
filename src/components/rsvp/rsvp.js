74f77778d25e72d7f99558c576c84128ed2b73a3
Michael Shing <michael.shing@bt.com>
2018-04-17 11:27:31 +0100

    moreee

--- src/components/rsvp/rsvp.js
+++ src/components/rsvp/rsvp.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
 import { Form, Button } from 'reactstrap';
 
 import './rsvp.scss';
 
 class Rsvp extends Component {
-  static propTypes = {
-    guest: PropTypes.string.isRequired
-  };
-
   static defaultProps = {
     guest: ''
   };
@@ -20,19 +14,6 @@ class Rsvp extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // async handleSubmit(e) {
-  //   e.preventDefault();
-
-  //   const response = e.target.name;
-  //   const { guest } = this.props;
-
-  //   /* eslint-disable */
-  //   const rsvp = await axios.post('/api/rsvp', {
-  //     guest,
-  //     response
-  //   });
-  // }
-
   encode = (data) => {
     return Object.keys(data)
       .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
@@ -40,10 +21,15 @@ class Rsvp extends Component {
   }
 
   handleSubmit = (e) => {
+    const response = e.target.name;
+
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: this.encode({ 'form-name': 'contact', ...this.state })
+      body: this.encode({
+        'form-name': 'contact',
+        response
+      })
     })
       .then(() => console.log('Success!'))
       .catch(error => console.log(error));
