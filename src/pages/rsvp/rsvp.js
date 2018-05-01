82abab3567cf0e2e1892d48a0ae5bead0e86279e
Michael Shing <michael.shing@bt.com>
2018-05-01 17:03:40 +0100

    boom

--- src/pages/rsvp/rsvp.js
+++ src/pages/rsvp/rsvp.js
@@ -4,6 +4,18 @@ import Page from '../../components/page/page';
 import Rsvp from '../../components/rsvp/rsvp';
 
 class RsvpView extends Component {
+
+  componentDidMount() {
+    fetch('https://api.netlify.com/api/v1/sites/3ff4b4ce-bfe2-4f82-8e4e-878af64d153e/forms')
+      .then((response) => {
+        console.log(response);
+        response.json();
+      })
+      .then((res) => {
+        // console.log(res.json());
+      });
+  }
+
   render() {
     const header = false;
 
