992651ac9dd854923510e224d0dbfe69ddd46cb8
Michael Shing <michael.shing@bt.com>
2018-05-02 00:41:51 +0100

    Mffddfdfdsfdsfdknkkkkknfdfdsfds

--- functions/fetch-rsvps.js
+++ functions/fetch-rsvps.js
@@ -3,8 +3,8 @@ const https = require('https');
 exports.handler = function (event, context, callback) {
   const token = process.env.NETLIFY_KEY;
   const siteId = process.env.SITE_ID;
-  const apiQuery = 'submissions?access_token=';
-  const baseApiUrl = 'https://api.netlify.com/api/v1/sites';
+  // const apiQuery = 'submissions?access_token=';
+  // const baseApiUrl = 'https://api.netlify.com/api/v1/sites';
 
   const options = {
     hostname: 'api.netlify.com',
