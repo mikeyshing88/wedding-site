b74d4f3cc9d53bcfcc079159d1d687747c039fdc
Michael Shing <michael.shing@bt.com>
2018-05-01 22:59:50 +0100

    Mffddfdfdsfdsfd

--- functions/fetch-rsvps.js
+++ functions/fetch-rsvps.js
@@ -10,25 +10,9 @@ exports.handler = function (event, context, callback) {
 
   console.log(apiUrl, 'API');
 
-  if (event.httpMethod !== 'POST') {
-    return callback(null, {
-      statusCode: 410,
-      body: 'Unsupported Request Method'
-    });
-  }
-  try {
-    const payload = JSON.parse(event.body);
-    console.log(payload, 'PAYLOAD');
-    fetch(apiUrl, {
-      method: 'POST',
-      body: JSON.stringify({ text: payload.text })
-    }).then((res) => {
+  fetch(apiUrl)
+    .then((res) => {
       console.log(res, 'fldjfkdjflj');
-      callback(null, { statusCode: 204 });
-    }).catch((e) => {
-      callback(null, { statusCode: 500, body: `Internal Server Error: ${e}` });
+      console.log(res.json(), 'HHISHISHS');
     });
-  } catch (e) {
-    callback(null, { statusCode: 500, body: `Internal Server Error: ${e}` });
-  }
 };
